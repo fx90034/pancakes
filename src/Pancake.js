76b692f769b85c0dbfbbdb4e3cda20b378e32807
bagpipes <dylanswafflez@gmail.com>
2018-09-08 12:56:25 -0400

    boobies

--- src/Pancake.js
+++ src/Pancake.js
@@ -1,2 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+
+export class Pancake extends React.Component {
+	
+	render() {
+		var a = './wood.png';
+		return (
+			<img src={ this.props.imageUri}/>
+		);
+	}
+}
+
