e928b22b2d46937abcd8f76c9ee76897dcdc3993
bagpipes <dylanswafflez@gmail.com>
2018-09-17 20:31:30 -0700

    resetserverjs

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var cors = require('cors');
-const path = require('path');
 var request = require('request');
 var cheerio = require('cheerio');
 
 var app = express();
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.get('/api/:url', function(req, res){
   request('https://stackoverflow.com/questions/'+req.params.url, function (error, response, body) {
